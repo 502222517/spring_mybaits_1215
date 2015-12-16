955b580e90cf0906564997fc2190e3bb7333e2d3
zhh <502222517@qq.com>
2015-12-16 11:25:10 +0800

    mybatis和spring整合-mapper代理开发

---
+++ src/cn/itcast/ssm/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package cn.itcast.ssm.mapper;
+
+import java.util.List;
+
+import cn.itcast.ssm.po.User;
+import cn.itcast.ssm.po.UserCustom;
+import cn.itcast.ssm.po.UserQueryVo;
+
+public interface UserMapper {
+	
+	public User findUserById(int id) throws	Exception;
+ 
+	 
+}
+
+
+
+
+
+
